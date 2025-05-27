fae447b7f9f251e9fb6a767c4ad6d76f42b14a61
BevenLiu <350379087@gapps.yrdsb.ca>
2025-05-27 08:14:08 -0400

    1

--- Brick.java
+++ Brick.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Brick extends Block
+public class Brick extends Actor
 {
     /**
      * Act - do whatever the brick wants to do. This method is called whenever
